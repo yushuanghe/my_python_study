e96d87c5866707aecf407fb8afe17c71645e8cd1
yushuanghe <yushuanghe@kingsoft.com>
2017-07-07 17:19:58 +0800

    测试demo

---
+++ demo1/demo1.py
@@ -0,0 +1,24 @@
+#!usr/bin/python
+# encoding:utf-8
+
+
+"""
+@author:yushuanghe
+@contact:yusuhanghe92@outlook.com
+@file:demo1.py
+@time:2017/06/23 00:41
+"""
+
+print u'大力出奇迹！'
+
+
+def a():
+    print u'测试a'
+
+
+def testa():
+    a()
+
+
+if __name__ == '__main__':
+    testa()
---
+++ demo1/demo2.py
@@ -0,0 +1,14 @@
+#!usr/bin/python
+# encoding:utf-8
+
+
+"""
+@author:yushuanghe
+@contact:yusuhanghe92@outlook.com
+@file:demo2.py
+@time:2017/06/23 00:45
+"""
+
+import demo1
+
+print u'二力不一般！'
